756a7a4436e2338cd1b64c94e17c392bc53746a3
Heedo Kim <devheedoo@gmail.com>
2019-09-17 17:37:58 +0900

    decorator와 함께 사용하기 완료

--- src/Counter.js
+++ src/Counter.js
@@ -6,10 +6,12 @@ import { observer } from 'mobx-react';
 class Counter extends Component {
   @observable number = 0;
 
+  @action
   increase = () => {
     this.number++;
   }
 
+  @action
   decrease = () => {
     this.number--;
   }
@@ -25,4 +27,13 @@ class Counter extends Component {
   }
 }
 
+// **** decorate 는 더 이상 필요 없어집니다.
+// decorate(Counter, {
+//   number: observable,
+//   increase: action,
+//   decrease: action
+// })
+
+// export default observer(Counter);
+// **** observer 는 코드의 상단으로 올라갑니다.
 export default Counter;

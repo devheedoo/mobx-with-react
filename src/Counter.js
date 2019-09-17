7969572faf54ecbf33f87de9e198c30d962c38d6
Heedo Kim <devheedoo@gmail.com>
2019-09-17 17:51:03 +0900

    inject로 컴포넌트에 스토어 주입

--- src/Counter.js
+++ src/Counter.js
@@ -1,39 +1,31 @@
 import React, { Component } from 'react';
-import { decorate, observable, action } from 'mobx';
-import { observer } from 'mobx-react';
+import { observer, inject } from 'mobx-react';
 
+@inject('counterStore')
 @observer
 class Counter extends Component {
-  @observable number = 0;
+  // @observable number = 0;
 
-  @action
-  increase = () => {
-    this.number++;
-  }
+  // @action
+  // increase = () => {
+  //   this.number++;
+  // }
 
-  @action
-  decrease = () => {
-    this.number--;
-  }
+  // @action
+  // decrease = () => {
+  //   this.number--;
+  // }
 
   render() {
+    const { counterStore } = this.props;
     return (
       <div>
-        <h1>{this.number}</h1>
-        <button onClick={this.increase}>+1</button>
-        <button onClick={this.decrease}>-1</button>
+        <h1>{counterStore.counter}</h1>
+        <button onClick={counterStore.increase}>+1</button>
+        <button onClick={counterStore.decrease}>-1</button>
       </div>
     )
   }
 }
 
-// **** decorate 는 더 이상 필요 없어집니다.
-// decorate(Counter, {
-//   number: observable,
-//   increase: action,
-//   decrease: action
-// })
-
-// export default observer(Counter);
-// **** observer 는 코드의 상단으로 올라갑니다.
 export default Counter;

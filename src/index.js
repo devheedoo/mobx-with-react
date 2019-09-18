4442ebf39eaf335ee9fe46ca6e8f366ce51208bc
Heedo Kim <devheedoo@gmail.com>
2019-09-18 09:45:28 +0900

    MarketStore 구현 및 적용

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import CounterStore from './stores/CounterStore';
+import MarketStore from './stores/MarketStore';
 
 const counterStore = new CounterStore();
+const marketStore = new MarketStore();
 
 ReactDOM.render(
-  <Provider counterStore={counterStore}>
+  <Provider counterStore={counterStore} marketStore={marketStore}>
     <App />
   </Provider>,
   document.getElementById('root')
